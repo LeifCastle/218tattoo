b5fe30e79baea26b6bf596fca86daaeffa04d3a6
Leif <leifurcastle@gmail.com>
2024-06-01 13:52:39 -0700

    STYLE: added media queries to better present design component on mobile, added a search bar without functionality)

--- components/designs.js
+++ components/designs.js
@@ -38,16 +38,26 @@ export default function Designs(props) {
     return (
         <>
             <div className={`${props.visibility === true ? 'block' : 'hidden'} fixed top-0 left-0 opacity-[40%] w-[100vw] h-[100vh] bg-black`} onClick={() => props.setVisibility(false)}></div>
-            <div className={`${props.visibility === true ? 'block' : 'hidden'} bg-greyB z-2 fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[40vw] h-[60vh] overflow-y-scroll rounded-lg`}>
-                <div className="absolute top-0 right-0 w-[50px] h-[50px] hover:scale-[1.25] duration-500" onClick={() => props.setVisibility(false)}>
-                    <div className="absolute top-0 right-0 translate-x-[-30px] translate-y-[15px] rotate-45 bg-blackA rounded w-[5px] h-[30px]"></div>
-                    <div className="absolute top-0 right-0 translate-x-[-30px] translate-y-[15px] rotate-[-45deg] bg-blackA rounded w-[5px] h-[30px]"></div>
+            <div className={`${props.visibility === true ? 'block' : 'hidden'} bg-greyB z-2 fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[90vw] Tablet:w-[60vw] h-[75vh] Tablet:h-[75vh] overflow-y-scroll rounded-lg`}>
+                <div className="flex items-center bg-white h-[60px] top-[-1px] sticky z-[4]">
+                <div className="flex Tablet:basis-1/3"></div>
+                    <div className="flex justify-center basis-1/3 pl-3 Tablet:pl-5">
+                        <p className="pr-3 Tablet:pr-5 text-lg Tablet:text-xl pt-1 text-black">Search:</p>
+                        <input placeholder="Eagles..." className="Mobile-S:max-w-[125px] Mobile-M:max-w-[175px] Mobile-L:max-w-fit text-lg Tablet:text-xl pl-1 pt-1 text-black bg-inputBorder border-gray-400 border-b-2 focus:border-inputBorder focus:outline-none hover:bg-inputHoverBg focus:bg-inputHoverBg"></input>
+                    </div>
+                    <div className="flex basis-1/3 overflow-x-clip">
+                        <div className="grow"></div>
+                        <div className="w-[50px] h-[60px] hover:scale-[1.25] duration-500" onClick={() => props.setVisibility(false)}>
+                            <div className="absolute top-0 right-0 translate-x-[-30px] translate-y-[17px] rotate-45 bg-blackA rounded w-[5px] h-[30px]"></div>
+                            <div className="absolute top-0 right-0 translate-x-[-30px] translate-y-[17px] rotate-[-45deg] bg-blackA rounded w-[5px] h-[30px]"></div>
+                        </div>
+                    </div>
                 </div>
-                <div className="py-[30px] flex flex-wrap gap-6 items-center justify-center">
+                <div className="pt-[55px] flex flex-wrap gap-6 items-center justify-center">
                     {designs.map(design => {
                         imageCounter++
                         return (
-                            <img className="rounded-lg hover:scale-[1.075] transition-all ease-in-out duration-500 cursor-pointer w-[25%] h-[25%]"
+                            <img className="rounded-lg hover:scale-[1.075] transition-all ease-in-out duration-500 cursor-pointer w-[42%] Tablet:w-[30%] h-[25%]"
                                 onClick={
                                     () => {
                                         props.setDesign(design)
