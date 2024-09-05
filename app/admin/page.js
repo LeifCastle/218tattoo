80e6a1a03e1c04ebf8b576d13c5eda9663af112e
Leif <leifurcastle@gmail.com>
2024-09-05 16:14:29 -0700

    FEAT: added admin functionality to cancel bookings and improved basic information show in an expanded bookings

--- app/admin/page.js
+++ app/admin/page.js
@@ -26,6 +26,7 @@ export default function Admin() {
     const [viewPort, setViewport] = useState('list')
     const [expanded, setExpanded] = useState(false)
     const [expandedBooking, setExpandedBooking] = useState(false)
+    const [bookingActionMessage, setBookingActionMessage] = useState(null)
 
     //Tailwind CSS Presets
     let inputName = "text-sm text-black"
@@ -68,10 +69,28 @@ export default function Admin() {
         }
     };
 
+    const cancelBooking = async (booking) => {
+        try {
+            console.log('Deleteing booking: ', {booking})
+            const response = await client.post('/admin/cancelBooking', { id : booking });
+            console.log('Response: ', response);
+            setBookingActionMessage('cancelled')
+            getBookings()
+            setTimeout(() => {
+                filterBookings('none')
+                setBookingActionMessage(null)
+                setExpanded(false)
+            }, 1250)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
     function getBookings() {
         return client.get('/admin')
             .then((response) => {
-                console.log('Found Booking: ', response.data);
+                console.log('Found Bookings: ', response.data);
                 bookings.current = response.data;
             })
             .catch(error => console.error("Failed to fetch bookings:", error));
@@ -136,7 +155,7 @@ export default function Admin() {
                             }}>Current</div>
                     </div>
                     {/**** List View ****/}
-                    <div id="test" className={`${panel === "listView" ? 'visible' : 'invisible'} relative flex items-start justify-center w-full p-10`}>
+                    <div id="test" className={`${panel === "listView" ? 'block' : 'hidden'} relative flex items-start justify-center w-full p-10`}>
                         <div className="bg-white rounded-md text-black flex flex-col items-center justify-center text-xl">
                             <div ref={bookingGrid} className="grid grid-cols-listView grid-rows-[50px] auto-rows-[40px] place-items-center border-pageGrey border-[1px] rounded">
                                 <div className="text-xl row-start-1 col-start-1 bg-pageGrey w-full h-full flex items-center justify-center">Service</div>
@@ -165,29 +184,77 @@ export default function Admin() {
                                 })}
                             </div>
                         </div>
-                        <div className={`${!expanded ? 'hidden' : 'block'} absolute top-0 left-0 w-full h-full min-h-[40vh] bg-pageGrey hover:pointer-default`}>
-                            <div className="bg-toggleSelected text-white text-4xl flex items-center justify-center">
-                                <Image className="hover:cursor-pointer"
-                                    src="/returnArrow.png"
-                                    width={50}
-                                    height={50}
-                                    alt="Return"
-                                    onClick={() => setExpanded(false)}
-                                />
+                        {/***********| Expanded Boooking View |***********/}
+                        <div className={`${!expanded ? 'hidden' : 'block'} absolute top-0 left-0 w-full h-full flex flex-col min-h-[40vh] bg-pageGrey text-black hover:pointer-default`}>
+                            <div className="bg-toggleSelected text-white text-3xl flex items-center justify-center">
+                                <div className='flex items-center justify-center gap-3 px-3 hover:cursor-pointer' onClick={() => setExpanded(false)}>
+                                    <Image className=""
+                                        src="/returnArrow.png"
+                                        width={50}
+                                        height={50}
+                                        alt="Return"
+                                    />
+                                    <p className='hover:cursor-pointer'>Return</p>
+                                </div>
                             </div>
-                            <div className='w-full h-full bg-pageGrey text-black'>
+                            <div className='flex flex-col'>
                                 <div className='w-full flex justify-center py-6'>
                                     <p className="text-5xl">{expandedBooking?.info?.contact?.firstName + " " + expandedBooking?.info?.contact?.lastName}</p>
                                 </div>
-                                <p className='pl-6 pb-3 text-2xl'>Service </p>
+                                {/*****| Service |*****/}
+                                <p className="pl-6 pb-3 text-2xl">{expandedBooking?.info?.service?.service}</p>
                                 <div className='flex'>
+                                    <div className='flex flex-col gap-2 shrink w-auto bg-white ml-10 p-4 rounded'>
+                                        {/* Count */}
+                                        <div className={`${expandedBooking?.info?.service?.service === "Tooth" ? 'block' : 'hidden'} flex`}>
+                                            <p className="text-xl pr-2">Gem Count: </p>
+                                            <p className="text-xl">{expandedBooking?.info?.service?.count}</p>
+                                        </div>
+                                        {/* Comments */}
+                                        <div className='flex'>
+                                            <p className="text-xl pr-2">{expandedBooking?.info?.service?.service === 'Tattoo' ? 'Description: ' : 'Comments: '}</p>
+                                            <p className="text-xl">{expandedBooking?.info?.service?.comments}</p>
+                                        </div>
+                                    </div>
+                                    <div></div>
+                                </div>
+                                {/*****| Appointment |*****/}
+                                <p className='pl-6 pb-3 text-2xl mt-10'>Appointment </p>
+                                <div className='flex'>
+                                    {/* Date & Time */}
                                     <div className='flex shrink w-auto bg-white rounded ml-10 p-4'>
-                                        <p className="text-xl">{expandedBooking?.info?.service?.service}</p>
-                                        <p className="text-xl mx-6">{moment(expandedBooking?.dateTime).format('MM/DD')}</p>
+                                        <p className="text-xl mr-4">{moment(expandedBooking?.dateTime).format('MM/DD')}</p>
                                         <p className="text-xl">{moment(expandedBooking?.dateTime).format('h:mm A')}</p>
                                     </div>
                                     <div></div>
                                 </div>
+                                {/*****| Contact |*****/}
+                                <p className='pl-6 pb-3 text-2xl mt-10'>Contact </p>
+                                <div className='flex'>
+                                    <div className='flex flex-col gap-2 shrink w-auto bg-white ml-10 p-4 rounded'>
+                                        {/* Phone */}
+                                        <div className={'flex'}>
+                                            <p className="text-xl pr-2">Phone: </p>
+                                            <p className="text-xl">{expandedBooking?.info?.contact?.phone}</p>
+                                        </div>
+                                        {/* Email */}
+                                        <div className='flex'>
+                                            <p className="text-xl pr-2">Email: </p>
+                                            <p className="text-xl">{expandedBooking?.info?.contact?.email}</p>
+                                        </div>
+                                    </div>
+                                    <div></div>
+                                </div>
+                            </div>
+                            <div className='grow'></div>
+                            {/*****| Cancel / Postpone |*****/}
+                            <div className='flex w-full text-center text-3xl font-medium text-[#444444]'>
+                                <div className='basis-1/2 bg-red-200 py-4 hover:cursor-pointer' onClick={() => cancelBooking(expandedBooking._id)}>Cancel</div>
+                                <div className='basis-1/2 bg-blue-200 py-4 hover:cursor-pointer'>Postpone</div>
+                            </div>
+                            {/*****| Booking Action Message |*****/}
+                            <div className={`${!bookingActionMessage ? 'hidden' : 'block'} absolute w-full h-full flex justify-center items-center bg-pageGrey`}> 
+                                <p className='text-4xl text-black'>Success! Booking Cancelled</p>
                             </div>
                         </div>
                     </div>
