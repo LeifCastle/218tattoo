183f602124c516de1319a51b405134e707ea360e
Leif <leifurcastle@gmail.com>
2024-09-28 19:27:29 -0700

    FIX: removed hash from CSP to allow 'unsafe-inline' to be active

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ export default {
             key: 'Content-Security-Policy',
             value: `
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://*.js.stripe.com https://checkout.stripe.com https://connect-js.stripe.com https://maps.googleapis.com;
-              style-src 'self' 'unsafe-inline' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=' https://js.stripe.com https://fonts.googleapis.com;
+              style-src 'self' 'unsafe-inline' https://js.stripe.com https://fonts.googleapis.com;
               frame-src 'self' https://checkout.stripe.com https://js.stripe.com/;
               connect-src 'self' https://checkout.stripe.com http://localhost:5000 https://tattoo218-backend-de45108a854b.herokuapp.com;
               img-src 'self' 'unsafe-inline' https://*.stripe.com https://marineagency.com https://png.pngtree.com;
