d7407fd73f19c747c105cac15cbc4bf93c4623bf
Leif <leifurcastle@gmail.com>
2024-09-28 18:57:49 -0700

    FIX: corrected server url in CSP

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ export default {
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://*.js.stripe.com https://checkout.stripe.com https://connect-js.stripe.com https://maps.googleapis.com;
               style-src 'self' 'unsafe-inline' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=' https://js.stripe.com https://fonts.googleapis.com;
               frame-src 'self' https://checkout.stripe.com https://js.stripe.com/;
-              connect-src 'self' https://checkout.stripe.com http://localhost:5000 https://tattoo218-d1912baeabec.herokuapp.com;
+              connect-src 'self' https://checkout.stripe.com http://localhost:5000 https://tattoo218-backend-de45108a854b.herokuapp.com/admin/login;
               img-src 'self' 'unsafe-inline' https://*.stripe.com https://marineagency.com https://png.pngtree.com;
               font-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com/;
             `.replace(/\n/g, ''),
